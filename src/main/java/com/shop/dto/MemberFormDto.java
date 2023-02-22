423e65b5653368b719acd7d0bfb3e40b42d39be6
pjnw1236 <pjnw1236@gmail.com>
2023-02-22 11:56:57 +0900

    feat: 회원가입 화면 및 controller 구성

--- src/main/java/com/shop/dto/MemberFormDto.java
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -2,6 +2,7 @@ package com.shop.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -17,6 +18,7 @@ public class MemberFormDto {
     private String email;
 
     @NotEmpty(message = "비밀번호는는 필수 입력 값입니다.")
+    @Length(min=8, max=16, message="비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
     @NotEmpty(message = "주소는 필수 입력 값입니다.")
