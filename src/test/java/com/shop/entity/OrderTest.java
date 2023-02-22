07e51cc2ba76f8dffd95f22f1bc79d6380590a66
pjnw1236 <pjnw1236@gmail.com>
2023-02-23 02:22:35 +0900

    feat: Entity 지연로딩 설정

--- src/test/java/com/shop/entity/OrderTest.java
+++ src/test/java/com/shop/entity/OrderTest.java
@@ -3,6 +3,7 @@ package com.shop.entity;
 import com.shop.constant.ItemSellStatus;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderItemRepository;
 import com.shop.repository.OrderRepository;
 import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.Assertions;
@@ -28,6 +29,8 @@ class OrderTest {
     ItemRepository itemRepository;
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    OrderItemRepository orderItemRepository;
     @PersistenceContext
     EntityManager em;
 
@@ -101,5 +104,19 @@ class OrderTest {
         Order order = this.createOrder();
         order.getOrderItems().remove(0);
         em.flush();
+    }
+
+    @Test
+    @DisplayName("지연 로딩 테스트")
+    public void lazyLoadingTest() {
+        Order order = this.createOrder();
+        Long orderItemId = order.getOrderItems().get(0).getId();
+        em.flush();
+        em.clear();
+
+        OrderItem orderItem = orderItemRepository.findById(orderItemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        Assertions.assertTrue(orderItem instanceof OrderItem);
     }
 }
