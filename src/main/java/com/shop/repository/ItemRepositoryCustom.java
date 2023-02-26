efa82105e3a5a4433229b2c3dc14841d60d2ef52
pjnw1236 <pjnw1236@gmail.com>
2023-02-27 04:27:24 +0900

    feat: 메인 화면 구성

--- src/main/java/com/shop/repository/ItemRepositoryCustom.java
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.shop.repository;
 
+import com.shop.dto.MainItemDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemSearchDto;
 import org.springframework.data.domain.Page;
@@ -7,4 +8,5 @@ import org.springframework.data.domain.Pageable;
 
 public interface ItemRepositoryCustom {
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
 }
