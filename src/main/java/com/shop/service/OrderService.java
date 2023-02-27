51176ef5f58e6d20fb4066415af174a64a3773d1
pjnw1236 <pjnw1236@gmail.com>
2023-02-27 22:52:09 +0900

    feat: 주문 취소 구현

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -67,4 +67,25 @@ public class OrderService {
 
         return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
     }
+
+    @Transactional(readOnly = true)
+    public boolean validateOrder(Long orderId, String email){
+        Member curMember = memberRepository.findByEmail(email);
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+        Member savedMember = order.getMember();
+
+        if(!StringUtils.equals(curMember.getEmail(), savedMember.getEmail())){
+            return false;
+        }
+
+        return true;
+    }
+
+    public void cancelOrder(Long orderId){
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+        order.cancelOrder();
+    }
+
 }
